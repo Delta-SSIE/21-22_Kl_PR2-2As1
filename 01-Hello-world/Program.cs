a5c0bbc345bfde70e766c02a046a50411b852605
Jan Koupil <jan.koupil@gmail.com>
2021-09-14 23:01:43 +0200

    2. lekce - opakovani syntaxe

--- 01-Hello-world/Program.cs
+++ 01-Hello-world/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 
-namespace _01_Hello_world
+namespace _01_Uvod_00_Opakovani_syntaxe
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            //int cislo = 100;
+
+            //string stav;
+            //if (cislo > 25)
+            //    stav = "velké";
+            //else
+            //    stav = "malé";
+
+            //string stav = cislo > 25 ? "velké" : "malé";
+
+            //string stav = cislo < 5 ? "prťavé"
+            //            : cislo < 7 ? "malé"
+            //            : cislo < 9 ? "tak akorát"
+            //            : "velké";
+
+
+
+            //Console.WriteLine($"Číslo je {stav}");
+
+            //for (int poradi = 0; poradi < 20; poradi++)
+            //{
+            //    Console.WriteLine(poradi);
+            //}
+
+            //int cislo = 100;
+            //while (cislo < 20)
+            //{
+            //    Console.WriteLine(cislo);
+            //    cislo++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(cislo);
+            //    cislo++;
+            //}
+            //while (cislo < 20);
+
+
+            //int cislo = 1;
+            //while (cislo < 30)
+            //{
+            //    cislo++;
+            //    if (cislo % 8 == 0)
+            //        continue;
+            //        //break;
+            //    Console.WriteLine(cislo);
+            //}
+
+            int x = 10;
+            int y = 20;
+
+            double quotient = (double)x / y;
+            Console.WriteLine($"Podíl {x} a {y} je {quotient}.");
         }
     }
 }
---
+++ 01-Uvod_01-Prohozeni-promennych/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01_Uvod_01_Prohozeni_promennych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            //dočasná proměnná
+
+            //int tmp = x;
+            //x = y;
+            //y = tmp;
+
+
+            //odčítání
+
+            //x = x + y;
+            //y = x - y;
+            //x = x - y;
+
+
+            //destructuring
+            //(x, y) = (y, x);
+
+
+            // XOR
+
+            x = x ^ y;
+            y = x ^ y;
+            x = x ^ y;
+
+            Console.WriteLine($"x: {x}, y: {y}");
+        }
+    }
+}
