4a891b03263e626ff8bf6cfb4193cc3ed41ccdf6
Jan Koupil <jan.koupil@gmail.com>
2021-09-21 10:18:57 +0200

    21.9. TryParse

---
+++ 01-Uvod-02-TryParse/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01_Uvod_02_TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Zadej číslo: ");
+
+            //string nacteno = Console.ReadLine();
+
+            //int cislo = Convert.ToInt32(nacteno);
+            //int cislo = int.Parse(nacteno);
+            //
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //double b = double.Parse(Console.ReadLine());
+
+            //int cislo;
+
+            //if (int.TryParse(nacteno, out cislo))
+            //{
+            //    Console.WriteLine($"Zadal jsi {cislo}.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("To není platné číslo!");
+            //}
+
+
+            Console.Write("Zadej kladné číslo: ");
+            string nacteno = Console.ReadLine();
+
+            double cislo;
+
+            while( !double.TryParse(nacteno, out cislo))
+            {
+                Console.Write("To není platné číslo, ňoumo! Zadej znovu: ");
+                nacteno = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Zadal jsi {cislo}.");
+
+        }
+    }
+}
