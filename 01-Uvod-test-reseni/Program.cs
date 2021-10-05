38b486a635c2ed435295f9758fd737cdc59959ce
Jan Koupil <jan.koupil@gmail.com>
2021-10-05 12:46:18 +0200

    5.10. 2021 Enumerace

---
+++ 01-Uvod-test-reseni/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Uvod_test_reseni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] slova = { "slovo", "dvě slova", "45dsa456ds56a45dsa" };
+
+        }
+
+        static bool JsouDelsiNez(string[] retezce, int limit)
+        {
+            for (int i = 0; i < retezce.Length; i++)
+            {
+                if (retezce[i].Length <= limit)
+                    return false;
+            }
+
+            return true;
+
+            foreach (string retezec in retezce)
+            {
+                if (retezec.Length <= limit)
+                    return false;
+            }
+            return true;
+
+        } 
+    }
+}
---
+++ 02-OOP-apod-Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02_OOP_apod_Enum
+{
+    enum Obdobi : byte { Jaro, Leto, Podzim, Zima }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obdobi chladne = Obdobi.Jaro;
+            Console.WriteLine(chladne);
+
+            Obdobi horke = chladne + 1;
+            Console.WriteLine(horke);
+
+            Obdobi divne = horke + 3;
+            Console.WriteLine(divne);
+
+            Obdobi test = (Obdobi)2;
+            Console.WriteLine(test);
+
+            //char pismeno = (char)('a' + 3);
+            //Console.WriteLine(pismeno);
+
+            Obdobi velke = (Obdobi)255;
+            velke++;
+            Console.WriteLine(velke);
+        }
+    }
+}
