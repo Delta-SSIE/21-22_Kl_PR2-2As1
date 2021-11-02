a0b884e4d726fa70789d7b6e84ee7d568ade9cfa
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-02 10:26:05 +0100

    Zaklad - kruznice

---
+++ 02-OOP-02-Kruznice-get-set/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02_OOP_02_Kruznice_get_set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kruznice k = new Kruznice();
+            k.Polomer = -1;
+
+            Console.WriteLine($"Kružnice o poloměru {k.Polomer} cm má obvod {k.SpoctiObvod():0.00} cm a obsah {k.SpoctiObsah():0.00} cm^2.");
+        }
+    }
+}
