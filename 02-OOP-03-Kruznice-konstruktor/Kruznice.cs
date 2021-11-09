5c318e216b6cd28744b2974e78e4b48d68bf1431
Jan Koupil <jan.koupil@gmail.com>
2021-11-09 13:19:42 +0100

    2021-09-11 Konstruktor + reseni predchozich uloh

---
+++ 02-OOP-03-Kruznice-konstruktor/Kruznice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOP_03_Kruznice_konstruktor
+{
+    
+    class Kruznice
+    {
+        private double _polomer;
+        public double GetPolomer()
+        {
+            return _polomer;
+        }
+        public void SetPolomer(double hodnota)
+        {
+            if (hodnota < 0)
+            {
+                //Console.WriteLine("Neplatná hodnota");
+                throw new ArgumentOutOfRangeException("Negative diameter");
+            }
+            else // když je to OK
+            {
+                _polomer = hodnota;
+            }
+        }
+
+        public Kruznice()
+        {
+            SetPolomer(0);
+        }
+
+        public Kruznice (double polomer)
+        {
+            SetPolomer(polomer);
+        }
+
+        public double SpoctiObvod()
+        {
+            return 2 * Math.PI * _polomer;
+        }
+
+        public double SpoctiObsah()
+        {
+            return Math.PI * _polomer * _polomer;
+        }
+    }
+}
---
+++ 02-OOP-03-Kruznice-konstruktor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02_OOP_03_Kruznice_konstruktor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Kruznice k = new Kruznice(1);
+                //k.SetPolomer(1);
+
+                Console.WriteLine($"Kružnice o poloměru {k.GetPolomer()} cm má obvod {k.SpoctiObvod():0.00} cm a obsah {k.SpoctiObsah():0.00} cm^2.");
+            }
+            catch
+            {
+                Console.WriteLine("Došlo k nečekané chybě");
+            }
+
+            Kruznice l = new Kruznice();
+           
+        }
+
+    }
+}
