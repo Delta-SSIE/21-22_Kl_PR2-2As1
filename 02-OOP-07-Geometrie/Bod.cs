b2d164e6aaa2343d4b322016966df5884f873b45
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-23 10:48:55 +0100

    2021-11-23 geometrie zaklad

---
+++ 02-OOP-07-Geometrie/Bod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOP_07_Geometrie
+{
+    class Bod
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Bod(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return $"[{X}, {Y}]";
+        }
+        public bool JeShodnyS(Bod jinyBod)
+        {
+            return this.X == jinyBod.X && this.Y == jinyBod.Y;
+        }
+        public double VzdalenostOd(Bod jinyBod)
+        {
+            double dx = this.X - jinyBod.X;
+            double dy = this.Y - jinyBod.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+    }
+}
---
+++ 02-OOP-07-Geometrie/Obdelnik.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOP_07_Geometrie
+{
+    class Obdelnik
+    {
+
+        private double _stranaA;
+        public double StranaA
+        {
+            get
+            {
+                return _stranaA;
+            }
+            private set
+            {
+                if (value >= 0)
+                    _stranaA = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        private double _stranaB;
+        public double StranaB
+        {
+            get
+            {
+                return _stranaB;
+            }
+            set
+            {
+                if (value >= 0)
+                    _stranaB = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        public Obdelnik()
+        { }//bez tohoto prázdného konstruktoru by nebylo možné vytvořit obdélník s ještě nedefinovanými stranami
+        public Obdelnik(double stranaA, double stranaB)
+        {
+            StranaA = stranaA; //používáme vlastnost i zevnitř konstruktoru - zajistí kontrolu přípustnosti hodnoty
+            StranaB = stranaB;
+        }
+
+        public double Obsah()
+        {
+            return _stranaA * _stranaB;
+        }
+        public double Obvod()
+        {
+            return 2 * (_stranaA + _stranaB);
+        }
+       
+        public override string ToString()
+        {
+            return $"Obdélník [{_stranaA}, {_stranaB}]";
+        }
+    }
+}
