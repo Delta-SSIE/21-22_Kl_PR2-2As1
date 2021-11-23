2b6a3ffc4ea7b90e2b5430dee0f7d1e61ea92501
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-23 11:24:49 +0100

    2021-11-24 geometrie - hotova

--- 02-OOP-07-Geometrie/Obdelnik.cs
+++ 02-OOP-07-Geometrie/Obdelnik.cs
@@ -9,56 +9,94 @@ namespace _02_OOP_07_Geometrie
     class Obdelnik
     {
 
-        private double _stranaA;
-        public double StranaA
+        private double _sirka;
+        public double Sirka
         {
             get
             {
-                return _stranaA;
+                return _sirka;
             }
             private set
             {
                 if (value >= 0)
-                    _stranaA = value;
+                    _sirka = value;
                 else
                     throw new ArgumentOutOfRangeException();
             }
         }
-        private double _stranaB;
-        public double StranaB
+        private double _vyska;
+        public double Vyska
         {
             get
             {
-                return _stranaB;
+                return _vyska;
             }
             set
             {
                 if (value >= 0)
-                    _stranaB = value;
+                    _vyska = value;
                 else
                     throw new ArgumentOutOfRangeException();
             }
         }
-        public Obdelnik()
-        { }//bez tohoto prázdného konstruktoru by nebylo možné vytvořit obdélník s ještě nedefinovanými stranami
-        public Obdelnik(double stranaA, double stranaB)
+        public Bod LevyHorni { get; private set; }
+        public Bod PravyHorni
         {
-            StranaA = stranaA; //používáme vlastnost i zevnitř konstruktoru - zajistí kontrolu přípustnosti hodnoty
-            StranaB = stranaB;
+            get
+            {
+                double x = LevyHorni.X + Sirka;
+                double y = LevyHorni.Y;
+                return new Bod(x, y);
+            }
+        }
+        public Bod LevyDolni
+        {
+            get
+            {
+                double x = LevyHorni.X;
+                double y = LevyHorni.Y + Vyska;
+                return new Bod(x, y);
+            }
+        }
+        public Bod PravyDolni
+        {
+            get
+            {
+                double x = LevyHorni.X + Sirka;
+                double y = LevyHorni.Y + Vyska;
+                return new Bod(x, y);
+            }
+        }
+
+        public Obdelnik(double sirka, double vyska, Bod levyHorniRoh)
+        {
+            Sirka = sirka; //používáme vlastnost i zevnitř konstruktoru - zajistí kontrolu přípustnosti hodnoty
+            Vyska = vyska;
+            LevyHorni = levyHorniRoh;
         }
 
         public double Obsah()
         {
-            return _stranaA * _stranaB;
+            return _sirka * _vyska;
         }
         public double Obvod()
         {
-            return 2 * (_stranaA + _stranaB);
+            return 2 * (_sirka + _vyska);
         }
        
         public override string ToString()
         {
-            return $"Obdélník [{_stranaA}, {_stranaB}]";
+            return $"Obdélník [{_sirka}, {_vyska}]";
+        }
+
+        public bool Obsahuje(Bod bod)
+        {
+
+            return bod.X >= LevyHorni.X
+                && bod.Y >= LevyHorni.Y
+                && bod.X <= PravyDolni.X
+                && bod.Y <= PravyDolni.Y;
+
         }
     }
 }
--- 02-OOP-07-Geometrie/Program.cs
+++ 02-OOP-07-Geometrie/Program.cs
@@ -6,33 +6,39 @@ namespace _02_OOP_07_Geometrie
     {
         static void Main(string[] args)
         {
+            //Bod A = new Bod(3, -1);
+            ////Console.WriteLine(A);
+
+            //Bod B = new Bod(5, 2);
+            //Bod C = new Bod(3, -1);
+
+            //if (A.JeShodnyS(B))
+            //{
+            //    Console.WriteLine("Body A a B se shodují");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Body A a B se liší");
+            //}
+
+            //if (A.JeShodnyS(C))
+            //{
+            //    Console.WriteLine($"Body {A} a {C} se shodují");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Body {A} a {C} se liší");
+            //}
+
+            //Console.WriteLine(A.VzdalenostOd(B));
+            //Console.WriteLine(A.VzdalenostOd(C));
+
             Bod A = new Bod(3, -1);
-            //Console.WriteLine(A);
-
-            Bod B = new Bod(5, 2);
-            Bod C = new Bod(3, -1);
-
-            if (A.JeShodnyS(B))
-            {
-                Console.WriteLine("Body A a B se shodují");
-            }
-            else
-            {
-                Console.WriteLine("Body A a B se liší");
-            }
-
-            if (A.JeShodnyS(C))
-            {
-                Console.WriteLine($"Body {A} a {C} se shodují");
-            }
-            else
-            {
-                Console.WriteLine($"Body {A} a {C} se liší");
-            }
-
-            Console.WriteLine(A.VzdalenostOd(B));
-            Console.WriteLine(A.VzdalenostOd(C));
-
+            Obdelnik ABCD = new Obdelnik(2, 6, A);
+            Console.WriteLine(ABCD.LevyHorni);
+            Console.WriteLine(ABCD.PravyHorni);
+            Console.WriteLine(ABCD.LevyDolni);
+            Console.WriteLine(ABCD.PravyDolni);
         }
     }
 }
