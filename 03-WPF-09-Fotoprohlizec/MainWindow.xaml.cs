7670f2bfe5864b628c4e7d40327704b2d2222e34
Jan Koupil <jan.koupil@delta-skola.cz>
2022-01-04 08:51:05 +0100

    Fotoprohlizec

--- 03-WPF-09-Fotoprohlizec/MainWindow.xaml.cs
+++ 03-WPF-09-Fotoprohlizec/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+
 namespace _03_WPF_09_Fotoprohlizec
 {
     /// <summary>
@@ -23,6 +25,32 @@ namespace _03_WPF_09_Fotoprohlizec
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Please select image";
+            ofd.Filter =  "All supported formats|*.jpg;*.jpeg;*.png|"
+                        + "JPEG image (*.jpg;*.jpeg)|*.jpg;*.jpeg|"
+                        + "Portable network graphics (*.png)|*.png";
+            if (ofd.ShowDialog() == true)
+            {
+                FilenameLbl.Content = ofd.FileName;
+                try
+                {
+                    PhotoImg.Source = new BitmapImage(new Uri(ofd.FileName));
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid file", 
+                        "File format not valid", 
+                        MessageBoxButton.OK, 
+                        MessageBoxImage.Error
+                    );
+                }
+
+            }
         }
     }
 }
