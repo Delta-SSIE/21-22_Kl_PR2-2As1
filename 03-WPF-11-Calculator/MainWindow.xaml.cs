9e57ec1c8a8a05be2812c0b6eaf22926fee4c1e3
Jan Koupil <jan.koupil@gmail.com>
2022-01-25 11:20:19 +0100

    2022-01-25 Calculator - první tlačítka

--- 03-WPF-11-Calculator/MainWindow.xaml.cs
+++ 03-WPF-11-Calculator/MainWindow.xaml.cs
@@ -20,9 +20,76 @@ namespace _03_WPF_11_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        //uložená data
+        //aktuální stav displeje
+        private string currentText = "0";
+
+        //minulá vložená hodnota
+        private double lastValue = 0;
+
+        //zvolená operace
+        private SimpleMath.Operation operation;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void NumberBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //odkaz na tlačítko, které bylo stisknuto
+            Button pressed = (Button)sender;
+
+            //vytáhnu číslici tlačítka
+            string symbol = pressed.Content.ToString();
+
+            //vezmi, co je na displeji
+            if (currentText == "0")
+            {
+                currentText = "";
+            }
+
+            //přidej za to stisknutou číslici
+            currentText += symbol;
+
+            //zobraz
+            Render();
+        }
+
+        private void Render()
+        {
+            Display.Text = currentText;
+        }
+
+        private void ACBtn_Click(object sender, RoutedEventArgs e)
+        {
+            currentText = "0";
+            Render();
+        }
+
+        private void PlusMinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentText == "0") //nule nebudu obracet znaménko
+            {
+                return;
+            }
+
+            //zjistíme, jestli je první znak "-"
+            char first = currentText[0];
+
+            if (first != '-') //když není na začítku mínus
+            { 
+                currentText = "-" + currentText; //přidej na začátek -
+            }
+            else
+            {
+                currentText = currentText.Substring(1); //zahoď první znak
+            }
+
+            //jiný zápis - převod na číslo a zpět na řetězec
+            //currentText = Convert.ToString(double.Parse(currentText) * -1);
+
+            Render();
         }
     }
 }
